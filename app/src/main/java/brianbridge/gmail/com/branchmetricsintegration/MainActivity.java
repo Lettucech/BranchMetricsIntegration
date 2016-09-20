7b8fd189d0cba09e16d98093e4907b80e43ddab1
brianbridge <pingkiuho@gmail.com>
2016-09-20 21:19:28 +0800

    basic integrated

--- app/src/main/java/brianbridge/gmail/com/branchmetricsintegration/MainActivity.java
+++ app/src/main/java/brianbridge/gmail/com/branchmetricsintegration/MainActivity.java
@@ -1,13 +1,40 @@
 package brianbridge.gmail.com.branchmetricsintegration;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONObject;
+
+import io.branch.referral.Branch;
+import io.branch.referral.BranchError;
 
 public class MainActivity extends AppCompatActivity {
+	public static final String TAG = MainActivity.class.getSimpleName();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+
+		Branch branch = Branch.getInstance();
+		branch.initSession(new Branch.BranchReferralInitListener(){
+			@Override
+			public void onInitFinished(JSONObject referringParams, BranchError error) {
+				Log.d(TAG, referringParams.toString());
+			}
+		}, this.getIntent().getData(), this);
+	}
+
+	@Override
+	protected void onNewIntent(Intent intent) {
+		super.onNewIntent(intent);
+		setIntent(intent);
 	}
 }
